c224f6e037d97b7772c6f59cf503160b554d2bc3
Mateusz Kasprzak <kacpak@gmail.com>
2016-02-22 23:53:34 +0100

    Dodano powiadomienia o wyłączeniu systemu

--- View/MainWindow.xaml.cs
+++ View/MainWindow.xaml.cs
@@ -1,6 +1,10 @@
-﻿using System.Windows;
+﻿using System;
+using System.Windows;
+using System.Windows.Forms;
 using SystemShutdown.Model;
 using SystemShutdown.ViewModel;
+using static System.Drawing.Icon;
+using static System.Reflection.Assembly;
 
 namespace SystemShutdown.View {
     /// <summary>
@@ -19,12 +23,36 @@ namespace SystemShutdown.View {
         private readonly ShutdownControlModel _shutdownControl;
 
         /// <summary>
+        /// Ikona powiadomień
+        /// </summary>
+        private readonly NotifyIcon _notifyIcon;
+
+        /// <summary>
         /// Tworzy nowy obiekt głównego okna
         /// </summary>
         public MainWindow() {
             InitializeComponent();
             _viewModel = (MainViewModel) DataContext;
             _shutdownControl = new ShutdownControlModel(this);
+            _notifyIcon = new NotifyIcon {
+                Visible = true,
+                Text = Title,
+                Icon = ExtractAssociatedIcon(GetEntryAssembly().ManifestModule.Name),
+                BalloonTipIcon = ToolTipIcon.Info
+            };
+            _notifyIcon.Click += (s, a) => WindowState = WindowState == WindowState.Minimized ? WindowState.Normal : WindowState.Minimized;
+        }
+
+        /// <summary>
+        /// Wyświetla powiadomienie
+        /// </summary>
+        /// <param name="text">Treść powiadomienia</param>
+        /// <param name="title">Tytuł powiadomienia</param>
+        /// <param name="timeout">Długość wyświetlania powiadomienia</param>
+        private void ShowNotification(string title = null, string text = null, int timeout = 1500) {
+            _notifyIcon.BalloonTipTitle = title;
+            _notifyIcon.BalloonTipText = text;
+            _notifyIcon.ShowBalloonTip(timeout);
         }
 
         /// <summary>
@@ -34,9 +62,44 @@ namespace SystemShutdown.View {
             if (_viewModel.IsShuttingDown)
                 _shutdownControl.StopShutdown();
             else
-                _shutdownControl.StartSystemShutdown(_viewModel.ShutdownTime, () => _viewModel.UpdateTimeLeft());
+                _shutdownControl.StartSystemShutdown(_viewModel.ShutdownTime, ShuttingDownUpdate);
 
             _viewModel.IsShuttingDown = !_viewModel.IsShuttingDown;
+        }
+
+        /// <summary>
+        /// Aktualizacja interfejsu przy uruchomionym odliczaniu do wyłączenia systemu
+        /// </summary>
+        private void ShuttingDownUpdate() {
+            _viewModel.UpdateTimeLeft();
+
+            // Czas pozostały do wyłączenia komputera
+            var t = _viewModel.ShutdownTimeLeft;
+
+            // Komunikat co pół godziny, co 5 minut jeśli zostało mniej niż 15 minut, co minutę jeśli jest mniej niż 5 minut
+            if (t.Seconds == 0 && (t.Minutes % 30 == 0 || t.Hours == 0 && (t.Minutes <= 15 && t.Minutes % 5 == 0 || t.Minutes <= 5))) {
+
+                // Jeśli komunikat był w przeciągu ostatnich 2 sekund - nie pokazuj go
+                if ((_timeLeftOnLastShutdownNotification - t).Duration() <= TimeSpan.FromSeconds(2)) return;
+
+                // Wyświetl powiadomienie
+                ShowNotification("Czas do wyłączenia", _viewModel.FormattedShutdownTimeLeft);
+
+                // Zapamiętaj czas do wyłączenia
+                _timeLeftOnLastShutdownNotification = t;
+            }
+        }
+
+        /// <summary>
+        /// Czas jaki pozostał do wyłączenia przy ostanim powiadomieniu
+        /// </summary>
+        private TimeSpan _timeLeftOnLastShutdownNotification;
+
+        /// <summary>
+        /// Zamknięcie aplikacji
+        /// </summary>
+        private void Window_Closing(object sender, System.ComponentModel.CancelEventArgs e) {
+            _notifyIcon.Dispose();
         }
     }
 }
